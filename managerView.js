24bbaa629ac2a03b6ecd4183777fe15065e9ce3c
jstudenski <jstudenski@gmail.com>
2018-02-18 17:39:37 -0600

    manager view added addInventory function

--- managerView.js
+++ managerView.js
@@ -46,55 +46,94 @@ function managerPrompt() {
         console.log("Option Not Defined");
   }
 
-  function viewProducts(){
-    console.log("1");
-
-  }
-
-  function lowInventory(){
-    //console.log("2");
-    var table = new Table({
-      head: ['ID', 'Item', 'Department', 'Price', 'QTY'], 
-      colWidths: [4, 15, 15, 7, 5],
-    });
-
-    connection.query("SELECT * FROM products WHERE stock_quantity < 5;", function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-        var item_id = res[i].item_id;
-        var product = res[i].product_name;
-        var department = res[i].department_name;
-        var price = res[i].price; 
-        var qty = res[i].stock_quantity;
-
-        table.push([item_id, color(department, product), color(department, department), price, qty]);
-
-      // storeItem = {
-      //   "name":color(department, product),
-      //   "value":item_id
-      // }
-      // items.push(storeItem);
-    }
-
-    console.log(table.toString()); 
-    });
 
 
-    
-
-
-  }
-
-  function addInventory(){
-    console.log("3");
-
-  }
-
-  function newProduct(){
-    console.log("4");
-
-  }  
   });
 } 
+
+
+function viewProducts(){
+  console.log("1");
+
+}
+
+function lowInventory(){
+  //console.log("2");
+  var table = new Table({
+    head: ['ID', 'Item', 'Department', 'Price', 'QTY'], 
+    colWidths: [4, 15, 15, 7, 5],
+  });
+
+  connection.query("SELECT * FROM products WHERE stock_quantity < 5;", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      var item_id = res[i].item_id;
+      var product = res[i].product_name;
+      var department = res[i].department_name;
+      var price = res[i].price; 
+      var qty = res[i].stock_quantity;
+
+      table.push([item_id, color(department, product), color(department, department), price, qty]);
+
+    // storeItem = {
+    //   "name":color(department, product),
+    //   "value":item_id
+    // }
+    // items.push(storeItem);
+  }
+
+  console.log(table.toString()); 
+  });
+}
+
+function addInventory(){
+
+  inquirer.prompt([
+    {
+      type: "text",
+      message: "Product name:",
+      name: "name"
+    },
+    {
+      type: "list",
+      message: "Department:",
+      choices: [
+        "Pokeballs",
+        "Potions",
+        "Items",
+        "Medicine",
+        "Repellents"
+      ],
+      name: "dept",
+    },
+    {
+      type: "text",
+      message: "Price:",
+      name: "price"
+    },
+    {
+      type: "text",
+      message: "Quantity:",
+      name: "qty"
+    }
+  ]).then(function(res) {
+
+    connection.query("INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('"+res.name+"', '"+res.dept+"', '"+res.price+"', '"+res.qty+"')", function(err, res) {
+      console.log(res.affectedRows + " product added!\n");
+    }); 
+
+  });
+
+
+
+}
+
+function newProduct(){
+  console.log("4");
+
+}  
+
+
+
 
 
 function color(department, text) {
@@ -121,81 +160,3 @@ function color(department, text) {
   return msg(text);
 
 }
-
-
-// var items = [];
-
-// var displayItems = function() {
-
-//   var gray = clc.xterm(8);
-//   var logo1 = clc.xterm(226);
-//   var logo2 = clc.xterm(27);
-//   console.log(gray("┌──────────────────────────────────────────────────┐"));
-//   console.log(gray("│       "+logo1(" _____     _      ")+logo2(" _____         _   ")+"      │"));
-//   console.log(gray("│       "+logo1("|  _  |___| |_ ___")+logo2("|     |___ ___| |_ ")+"      │"));
-//   console.log(gray("│       "+logo1("|   __| . | '_| -_")+logo2("| | | | .'|  _|  _|")+"      │"));
-//   console.log(gray("│       "+logo1("|__|  |___|_,_|___")+logo2("|_|_|_|__,|_| |_|  ")+"      │"));
-
-
-//   // create table
-//   var table = new Table({
-//       head: ['ID', 'Item', 'Department', 'Price', 'QTY'], 
-//       colWidths: [4, 15, 15, 7, 5],
-//       // chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''}
-//       chars: { 'top-left': '├' , 'top-right': '┤'}
-//   });
-   
-//   connection.query("SELECT * FROM products", function(err, res) {
-//     for (var i = 0; i < res.length; i++) {
-
-//       var item_id = res[i].item_id;
-//       var product = res[i].product_name;
-//       var department = res[i].department_name;
-//       var price = res[i].price; 
-//       var qty = res[i].stock_quantity;
-
-//       table.push([item_id, color(department, product), color(department, department), price, qty]);
-
-//       storeItem = {
-//         "name":color(department, product),
-//         "value":item_id
-//       }
-//       items.push(storeItem);
-//     }
-
-//     console.log(table.toString());
-//     //call;
-//     //resolve();
-//     customerPrompt();
-//   });
-
-// }
-
-
-// function color(department, text) {
-//   var msg;//  = clc.xterm(202).bgXterm(236);
-//   switch(department) {
-//     case 'Pokeballs':
-//       msg = clc.xterm(32);
-//       break;
-//     case 'Potions':
-//       msg = clc.xterm(133);
-//       break;
-//     case 'Item':
-//       msg = clc.xterm(40);
-//       break; 
-//     case 'Medicine':
-//       msg = clc.xterm(220);
-//       break;  
-//     case 'Repellents':
-//       msg = clc.xterm(196);
-//       break;         
-//     default:
-//       msg = clc.xterm(231);
-//   }
-//   return msg(text);
-
-// }
-
-
-
